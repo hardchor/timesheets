5444b357809097bf5ce9cdc5f285249344d0c506
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2016-05-16 09:23:18 +0100

    improve trigger logic

--- app/shared/actions/github.js
+++ app/shared/actions/github.js
@@ -1,13 +1,15 @@
+import { ALIASED } from '.';
 import githubAuth from '../../main/window/githubAuth';
 import getRepos from '../../main/api/getRepos';
 
+export const aliases = {};
+
 // Authenticate
-export const REQUEST_AUTHENTICATE_GITHUB = 'REQUEST_AUTHENTICATE_GITHUB';
 export const AUTHENTICATE_GITHUB = 'AUTHENTICATE_GITHUB';
 
 export function requestAuthenticateGithub() {
   return {
-    type: REQUEST_AUTHENTICATE_GITHUB,
+    type: ALIASED,
     meta: {
       trigger: AUTHENTICATE_GITHUB,
     },
@@ -19,17 +21,17 @@ export function authenticateGithub() {
     payload: githubAuth(),
   };
 }
+aliases[AUTHENTICATE_GITHUB] = authenticateGithub;
 
 
 // Get repos
-export const REQUEST_GET_GITHUB_REPOS = 'REQUEST_GET_GITHUB_REPOS';
 export const GET_GITHUB_REPOS = 'GET_GITHUB_REPOS';
 export const TRACK_GITHUB_REPO = 'TRACK_GITHUB_REPO';
 export const UNTRACK_GITHUB_REPO = 'UNTRACK_GITHUB_REPO';
 
 export function requestGetGithubRepos(accessToken) {
   return {
-    type: REQUEST_GET_GITHUB_REPOS,
+    type: ALIASED,
     payload: [accessToken],
     meta: {
       trigger: GET_GITHUB_REPOS,
@@ -42,6 +44,8 @@ export function getGithubRepos(accessToken) {
     payload: getRepos(accessToken),
   };
 }
+aliases[GET_GITHUB_REPOS] = getGithubRepos;
+
 export function trackGithubRepo(id) {
   return {
     type: TRACK_GITHUB_REPO,
