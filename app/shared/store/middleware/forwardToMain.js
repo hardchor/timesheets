cf382f45101192eee160c369751ce2df375da156
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2016-05-17 21:44:20 +0100

    don't forward router actions to main

--- app/shared/store/middleware/forwardToMain.js
+++ app/shared/store/middleware/forwardToMain.js
@@ -2,9 +2,11 @@ import { ipcRenderer } from 'electron';
 
 const forwardToMain = store => next => action => {
   if (
-    !action.meta ||
-    !action.meta.scope ||
-    action.meta.scope !== 'local'
+    action.type.substr(0, 2) !== '@@' && (
+      !action.meta ||
+      !action.meta.scope ||
+      action.meta.scope !== 'local'
+    )
   ) {
     ipcRenderer.send('redux-action', action);
 
