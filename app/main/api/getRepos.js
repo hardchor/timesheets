8c2b44918c87fae6a4a36c2e75dad84726a40c84
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2016-05-18 20:11:48 +0100

    basic pagination

--- app/main/api/getRepos.js
+++ app/main/api/getRepos.js
@@ -1,13 +1,37 @@
 import fetch from 'node-fetch';
+import parseLinkHeader from 'parse-link-header';
 import { status, json } from '../../shared/helpers/fetch';
 
+
+async function fetchPaginated(url, options) {
+  const response = await fetch(url, options);
+  const { next } = parseLinkHeader(response.headers.get('link'));
+  let nextResponse = [];
+  if (next && next.url) {
+    nextResponse = await fetchPaginated(next.url, options);
+  }
+
+  return [response, ...nextResponse];
+}
+
+const iterator = middleware => responses => Promise.all(
+  responses.map(response => middleware(response))
+);
+
+function jsonAggregator(responses) {
+  const aggregated = [].concat(...responses);
+
+  return aggregated;
+}
+
 export default function getRepos(accessToken) {
-  return fetch('https://api.github.com/user/repos?sort=updated', {
+  return fetchPaginated('https://api.github.com/user/repos?sort=updated', {
     method: 'GET',
     headers: {
       Authorization: `token ${accessToken}`,
     },
   })
-  .then(status)
-  .then(json);
+  .then(iterator(status))
+  .then(iterator(json))
+  .then(jsonAggregator);
 }
