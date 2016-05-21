27dcb8a96a4a2253e994704a551fb6a048078c84
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2016-05-21 05:23:42 +0100

    better grouped report

--- app/renderer/main/components/Reports/GroupedReport.jsx
+++ app/renderer/main/components/Reports/GroupedReport.jsx
@@ -1,24 +1,60 @@
 import React, { PropTypes } from 'react';
-import Report from './Report';
+import { DataTable, TableHeader } from 'react-mdl';
+import moment from 'moment';
 
-function GroupedReport({ groupedJobs }) {
+function GroupedReport({ groupedDates, groupedJobs }) {
+  const rows = Object.keys(groupedJobs).map(projectName => {
+    const jobsGroupedByDate = groupedJobs[projectName];
+    const durationsGroupedByDate = {};
+
+    Object.keys(jobsGroupedByDate).forEach(groupedFromDate => {
+      const jobs = jobsGroupedByDate[groupedFromDate];
+
+      // accumulate time per time group
+      const accumulatedTime = jobs.reduce(
+        (previous, current) => {
+          const duration = moment(current.endAt) - moment(current.startAt);
+          return previous + duration;
+        },
+        0
+      );
+
+      durationsGroupedByDate[groupedFromDate] = accumulatedTime;
+    });
+
+    return {
+      projectName,
+      ...durationsGroupedByDate,
+    };
+  });
+
+  function durationFormatter(accumulatedTime) {
+    const duration = moment.duration(accumulatedTime);
+    return accumulatedTime && duration.humanize();
+  }
+
   return (
-    <div>
-      {Object.keys(groupedJobs).map(projectName => {
-        const jobs = groupedJobs[projectName];
-
-        return (
-          <div>
-            <h4>{projectName}</h4>
-            <Report jobs={jobs} />
-          </div>
-        );
-      })}
-    </div>
+    <DataTable
+      shadow={0}
+      rows={rows}
+    >
+      <TableHeader name="projectName">Project</TableHeader>
+      {groupedDates.map(groupedDate => (
+        <TableHeader
+          key={groupedDate.groupedFromDate.toString()}
+          name={groupedDate.groupedFromDate.toString()}
+          cellFormatter={durationFormatter}
+          numeric
+        >
+          {groupedDate.groupedFromDate.format('L')}
+        </TableHeader>
+      ))}
+    </DataTable>
   );
 }
 
 GroupedReport.propTypes = {
+  groupedDates: PropTypes.array.isRequired,
   groupedJobs: PropTypes.object.isRequired,
 };
 
--- app/renderer/main/components/Reports/index.jsx
+++ app/renderer/main/components/Reports/index.jsx
@@ -1,28 +1,46 @@
 /* eslint-disable no-param-reassign */
 import React, { PropTypes } from 'react';
 import moment from 'moment';
-import {
-  Grid,
-  Cell,
-} from 'react-mdl';
+import { Grid, Cell } from 'react-mdl';
 import GroupedReport from './GroupedReport';
 
 function Reports({ job }) {
-  const fromDate = moment().subtract(1, 'weeks');
-  const toDate = moment();
+  const groupDatesBy = 'day';
+  const fromDate = moment().subtract(7, 'days').startOf(groupDatesBy);
+  const toDate = moment().endOf(groupDatesBy);
+
+  // group by day
+  const groupedDates = [];
+  const currentDate = moment(fromDate).endOf(groupDatesBy);
+  do {
+    const groupedFromDate = currentDate.clone().startOf(groupDatesBy);
+    const groupedToDate = currentDate.clone().endOf(groupDatesBy);
+    groupedDates.push({ groupedFromDate, groupedToDate });
+
+    currentDate.add(1, groupDatesBy);
+  } while (currentDate.isBefore(toDate));
+
   // filter by time
   const jobs = job.jobs.filter(jobData => {
     const endAt = jobData.endAt && moment(jobData.endAt);
-    return endAt >= fromDate && endAt <= toDate;
+    return endAt.isBetween(fromDate, toDate);
   });
 
   // group by project name
   const groupedJobs = jobs.reduce(
     (previous, current) => {
       const projectName = current.projectName || 'No project assigned';
+      const endAt = current.endAt && moment(current.endAt);
 
-      previous[projectName] = previous[projectName] || [];
-      previous[projectName].push(current);
+      previous[projectName] = previous[projectName] || {};
+
+      groupedDates.forEach(({ groupedFromDate, groupedToDate }) => {
+        if (endAt.isBetween(groupedFromDate, groupedToDate)) {
+          const id = groupedFromDate.toString();
+          previous[projectName][id] = previous[projectName][id] || [];
+          previous[projectName][id].push(current);
+        }
+      });
 
       return previous;
     },
@@ -34,8 +52,11 @@ function Reports({ job }) {
       <Cell col={12}>
         <h1>Reports</h1>
         <small>{fromDate.calendar()} - {toDate.calendar()}</small>
-
-        <GroupedReport groupedJobs={groupedJobs} />
+        <GroupedReport
+          groupDatesBy={groupDatesBy}
+          groupedDates={groupedDates}
+          groupedJobs={groupedJobs}
+        />
       </Cell>
     </Grid>
   );
